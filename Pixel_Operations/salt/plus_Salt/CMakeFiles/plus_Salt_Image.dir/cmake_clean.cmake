cebb04d0aaa1fcf92ae42cf49dc185c2488ee73a
LiuJLin <Liu_JLin@outlook.com>
2015-12-24 11:42:13 +0800

    Pixel_Operations

---
+++ Pixel_Operations/salt/plus_Salt/CMakeFiles/plus_Salt_Image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/plus_Salt_Image.dir/plus_Salt_Image.cpp.o"
+  "plus_Salt_Image.pdb"
+  "plus_Salt_Image"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/plus_Salt_Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
